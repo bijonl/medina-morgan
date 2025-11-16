f3301b4fba7aa42555e76258fe2fb0dc853e9c63
bijonl <bijon2591@gmail.com>
2025-11-16 11:43:03 -0500

    staff hero area

--- precious-works-theme-child/components/staff/staff-hero.php
+++ precious-works-theme-child/components/staff/staff-hero.php
@@ -1,37 +1,46 @@
 <?php 
-include(locate_template('blocks/partials/global-block-variables.php')); 
-
-$section_title = get_field('section_title', false, false);
-$section_subtitle = get_field('section_subtitle');
-$section_title_tag = get_field('section_title_tag');
-$has_title_area = !empty($section_title) || !empty($section_subtitle); 
 
 
-$image = get_field('hero_image'); 
-$image_url = wp_get_attachment_url($image); 
-$interior_hero_type = get_field('interior_hero_type');
-$half_size = $interior_hero_type == 'half-sized'; 
-
-$has_content = $image || $has_button_area || $has_title_area;
-
-$hero_alignment = 'text-center';
-if($half_size) {
-    $hero_alignment = 'text-start'; 
-}  ?>
+$hero_alignment = 'text-start';
+ ?>
 
 <section class="single-staff-hero-area" style="background-image: url('<?php echo $staff_banner_image['url'] ?>')">
-    <?php if($half_size) { ?>
-        <div class="overlay image-overlay"></div>
-    <?php } ?>
-    <div class="homepage-hero-container interior-hero <?php echo $interior_hero_type ?> container h-100">
+
+    <div class="homepage-hero-container staff-hero container h-100">
      
-        <div class="homepage-hero-row interior-hero row align-items-center h-100">
-            <div class="homepage-hero-col interior-hero col-12">
-                <div class="homepage-hero-content-wrapper interior-hero <?php echo $hero_alignment ?>">
-                    <div class="overlay image-overlay"></div>
-                    <?php $display_title = 'h1'; ?>
-                    <?php include(locate_template('blocks/partials/title-area.php')); ?>
-                    <?php include(locate_template('blocks/partials/button-area.php')); ?>
+        <div class="homepage-hero-row staff-hero row justify-content-end align-items-center h-100">
+            <div class="homepage-hero-col staff-hero col-md-7 col-lg-7 col-xl-6">
+                <div class="staff-hero-content-wrapper background-primary staff-hero-content <?php echo $hero_alignment ?>">
+                    <div class="name-wrapper">
+                        <h1 class="h2"><?php echo $full_name ?></h1>
+                    </div>
+                    <div class="position-wrapper">
+                        <h3><?php echo $position_display_name ?></h3>
+                    </div>
+                    <div class="contact-wrapper d-xl-flex justify-content-between align-items-center">
+                        <div class="contact-info-wrapper">
+                            <p class="mb-0 phone">
+                                <a 
+                                class="color-inherit"
+                                href="tel:<?php echo preg_replace('/[^0-9+]/', '', $phone_number); ?>"
+                                aria-label="Call us at <?php echo $phone_number; ?>">
+                                    <?php echo $phone_number; ?>
+                                </a>
+                            </p>
+                            <p class="mb-0 email">
+                                <a class="color-inherit"
+                                href="mailto:<?php echo $email_address; ?>"
+                                aria-label="Email us at <?php echo $email_address; ?>">
+                                    <?php echo $email_address; ?>
+                                </a>
+                            </p>
+                        </div>
+                        <div class="button-email-wrapper">
+                            <a class="pw-solid-button text-center">Email <?php echo $first_name ?></a>
+                        </div>
+
+                    </div>
+               
                 </div>
             </div>
         </div>
